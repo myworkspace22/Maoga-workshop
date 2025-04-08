a869be0cd198f7944c54b77f3534cdb6eea820a4
Ribbo <61548182+myworkspace22@users.noreply.github.com>
2025-04-08 19:39:51 +0200

    Setup

---
+++ startpage/code.js
@@ -0,0 +1,11 @@
+console.clear();
+
+document.addEventListener("DOMContentLoaded", function() {
+    const img = document.getElementById("startpageImg");
+  
+    // Attach a click event to the image
+    img.addEventListener("click", function() {
+      // Redirect to the desired page
+      window.location.href = "page1.html";
+    });
+  });
